d65f42b8e034ae28a5b543bd768d07c0e40675f5
andrew <andrewmatosdiaz@hotmail.com>
2018-11-06 11:52:26 -0400

    I4 extensions

--- web_guesser.rb
+++ web_guesser.rb
@@ -7,13 +7,13 @@ require 'sinatra/reloader'
 SECRET_NUMBER = number
 
 def check_guess(guess,number)
-	if guess == nil or guess== ""
+	if guess == nil or guess== "" or guess == "restart"
 	   message =nil
 	elsif guess.to_i > number
 		if guess.to_i > number + 5
 	    message ="Way too high!"
 		else
-		message =" too high!"
+		message ="Too high!"
 	    end
 	elsif guess.to_i < number
         if guess.to_i < number - 5
@@ -27,8 +27,32 @@ def check_guess(guess,number)
 return message
 end
 
+@@turns=6
+
 get '/' do
-	guess = params["guess"]
+	
+    guess = params["guess"]
 	message = check_guess(guess,SECRET_NUMBER)
-	erb :index, :locals=>{:number=> number,:message => message}
+    
+    if @@turns == 0 && message !="You got it right!"
+    number = x.rand(10..100)
+    SECRET_NUMBER = number
+    @@turns=5	
+    end
+
+    if message !="You got it right!"
+	  @@turns-=1
+    end
+
+	if guess == "restart"
+	 number = x.rand(10..100)
+	 SECRET_NUMBER = number
+	 @@turns=5
+   end
+	
+    
+    cheat = params["cheat"]
+
+    erb :index, :locals=>{:number=> number,:message => message,:turns => @@turns,:cheat=>cheat}
 end
+
